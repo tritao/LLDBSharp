90cf8c20ec7896c4edfb6994c71dc6a842c6b591
João Matos <joao@tritao.eu>
2015-07-02 19:25:30 +0100

    Work-in-progress Mono SDB integration.

--- Example.cs
+++ Example.cs
@@ -1,15 +1,39 @@
 using LLDB;
+using Mono.Debugging.Client;
+using Mono.Debugging.Soft;
 using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading.Tasks;
+using System.Net;
 
 namespace LLDBSharp
 {
 	public class Program
 	{
+		static SoftDebuggerSession session;
+
+		static void ConnectSdb()
+		{
+			session = new SoftDebuggerSession ();
+
+			var address = IPAddress.Parse("127.0.0.1");
+			var port = 12345;
+
+			var args = new SoftDebuggerConnectArgs (string.Empty, address, port)
+			{
+				MaxConnectionAttempts = 10,
+				TimeBetweenConnectionAttempts = 1
+			};
+
+			var options = new DebuggerSessionOptions ();
+
+			session.Run(new SoftDebuggerStartInfo(args), options);
+		}
+
 		static void Main(string[] args)
 		{
 			var xcodePath = CppSharp.XcodeToolchain.GetXcodePath();
@@ -25,8 +49,13 @@ namespace LLDBSharp
 
 			var exe = "mono";
 			var exeArgs = new List<string> {
+				"--debug",
+				"--debugger-agent=transport=dt_socket,address=0.0.0.0:12345,server=y",
 				"Managed.exe",
 			};
+
+			var sdb = new System.Threading.Thread(ConnectSdb);
+			sdb.Start ();
 
 			Console.WriteLine ("Creating a target for {0}", exe);
 			var target = debugger.CreateTarget(exe);
@@ -34,7 +63,7 @@ namespace LLDBSharp
 			if (target == null)
 				throw new Exception ("Could not create LLDB target");
 
-			var mainBreakpoint = target.BreakpointCreateByName ("NativeFunc", target.GetExecutable().Filename);
+			var mainBreakpoint = target.BreakpointCreateByName ("NativeFunc", null);
 
 			var currentDir = Directory.GetCurrentDirectory ();
 			var error = new LLDB.Error();
@@ -47,10 +76,13 @@ namespace LLDBSharp
 			var stringArgsArr = stringArgs.ToArray ();
 			unsafe {
 				fixed (IntPtr* ptr = stringArgsArr) {
-					process = target.Launch (debugger.GetListener (), (sbyte**)ptr, null, null, null, null, currentDir,
-						0, false, error);
+					process = target.Launch (debugger.GetListener (), (sbyte**)ptr, null, null, null, null,
+						currentDir, 0, false, error);
 				}
 			}
+
+			foreach (var arg in stringArgs)
+				Marshal.FreeHGlobal (arg);
 
 			if (process == null || error.ErrorCode != 0)
 				throw new Exception (string.Format("Could not create LLDB process: {0}", error.CString));
@@ -62,12 +94,24 @@ namespace LLDBSharp
 
 			for (uint threadIndex = 0; threadIndex < process.NumThreads; ++threadIndex) {
 				var thread = process.GetThreadAtIndex (threadIndex);
-
+				Console.WriteLine ("\nThread #{0}:", threadIndex);
+					
 				for (uint frameIndex = 0; frameIndex < thread.NumFrames; ++frameIndex) {
 					var frame = thread.GetFrameAtIndex (frameIndex);
 					var function = frame.GetFunction ();
 					var symbol = frame.GetSymbol ();
-					Console.WriteLine ("\t{0}", function.Name ?? symbol.Name);
+
+					var pc = frame.GetPCAddress ();
+					var addr = pc.GetLoadAddress (target);
+					var expr = string.Format ("(char*)mono_pmip((void*){0})", addr);
+					var value = frame.EvaluateExpression (expr);
+
+					string frameName = null;
+					if (value.IsValid () && value.GetValueAsUnsigned (0) != 0)
+						error = ReadCStringFromValue (value, ref frameName);
+
+					var displayName = function.Name ?? symbol.Name ?? frameName ?? "(unknown)";
+					Console.WriteLine ("\t{0}", displayName);
 				}
 			}
 
@@ -86,6 +130,25 @@ namespace LLDBSharp
 			error = process.Continue ();
 
 			mainBreakpoint.Dispose ();
+		}
+
+		static Error ReadCStringFromValue (Value value, ref string @string)
+		{
+			var error = new Error ();
+			if (value.IsValid () && value.GetValueAsUnsigned (0) != 0) {
+				var sb = new StringBuilder ();
+				uint i = 0;
+				while (true) {
+					var @char = value.GetPointeeData (i++, 1).GetUnsignedInt8 (error, 0);
+					if (error.ErrorCode != 0)
+						break;
+					if (@char == '\0')
+						break;
+					sb.Append ((char)@char);
+				}
+				@string = sb.ToString ();
+			}
+			return error;
 		}
 	}
 }
