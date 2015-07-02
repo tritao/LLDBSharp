c277c0f156e7880c6220c3c4def959464e1521c4
Marius Ungureanu <marius.ungureanu@xamarin.com>
2015-07-02 18:48:19 +0100

    Improve test code.

--- tests/Managed.cs
+++ tests/Managed.cs
@@ -6,14 +6,14 @@ public class Program
 	const string NativeLib = "NativeLib.dll";
 
     [DllImport(NativeLib)]
-    public static extern void NativeFunc();
+    public static extern void NativeFunc(int x);
 
 	static void Main(string[] args)
 	{
 		Console.WriteLine("Hello world from managed!");
 		Console.WriteLine("Calling into native code...");
 
-		NativeFunc();
+		NativeFunc(42);
 
 		Console.WriteLine("Back into managed code.");
 	}
--- tests/Native.cpp
+++ tests/Native.cpp
@@ -9,8 +9,24 @@
 extern "C"
 {
 
-DLL_API void NativeFunc()
+DLL_API struct Test {
+public:
+	int a;
+
+	void test(int val) {
+		this->a = val;
+	}
+};
+
+DLL_API void NativeFunc(int x)
 {
+	Test c;
+	c.test(5);
+
+	int b[] = { 2, 4 };
+	int a = 5;
+	printf("%d", a);
+	a = x;
 	printf("Hello world from native!\n");
 	printf("Returning back to managed code...\n");
 }
